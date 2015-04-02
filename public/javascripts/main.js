0cc3e5e627724755600e5c0804305a4b827b7ef6
Andreas Moller <andreasm@CorpITs-MacBook-Pro-3.local>
2015-04-02 16:24:46 +0100

    update

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,14 +1,27 @@
 (function () {
   'use strict';
+  var replies = [
+    'Have you tried turning it off, and on again?',
+    'Is it plugged in?',
+    'Have you tried googling it?'
+  ];
+
   setup();
+
   function setup () {
     $('#speak').on('submit', speak);
   }
+
   function speak () {
-    var message = $('[name=message]').val();
-    $.post('/speak', {message: message}, function (res) {
-      console.log(res);
-    });
-    return false; // to prevent for submit!
+    var player = document.getElementById('player');
+    player.src = getRandomReplyUrl();
+    player.load();
+    player.play();
+    return false;
   }
+  function getRandomReplyUrl () {
+    var i = Math.round(Math.random() * (replies.length -1));
+    return 'http://tts-api.com/tts.mp3?q=' + encodeURIComponent(replies[i]);
+  }
+
 })();
--- routes/speak.js
+++ routes/speak.js
@@ -1,35 +1,17 @@
 'use strict';
 var say = require('say');
-var gpio = require('rpi-gpio');
-var exec = require('exec');
-var querystring = require('querystring');
-var LED1 = 12;
-var LED2 = 16;
-gpio.setup(LED1, gpio.DIR_OUT);
-gpio.setup(LED2, gpio.DIR_OUT);
+var replies = [
+  'Have you tried turning it off, and on again?',
+  'Is it plugged in?',
+  'Have you tried googling it?'
+];
 
 module.exports = function (req, res, next) {
-  var message = req.body.message;
-  console.log('SPEAK');
-  console.log(message);
-  if (!message) {
-    return res
-      .status(400)
-      .send('No message');
-  }
-  // lightsOn();
-  exec(['mplayer', 'http://tts-api.com/tts.mp3?' + querystring.stringify({q:message})], function () {
-    // lightsOff()  ;
-});
+  console.log('SPEAKING');
+  say.speak('Alex', getRandomReply());
   return res.send('Speaking');
 };
-
-function lightsOn () {
-  gpio.write(LED1, true);
-  gpio.write(LED2, true);
-}
-
-function lightsOff () {
-  gpio.write(LED1, true);
-  gpio.write(LED2, true);
+function getRandomReply () {
+  var i = Math.round(Math.random() * (replies.length -1));
+  return replies[i];
 }
